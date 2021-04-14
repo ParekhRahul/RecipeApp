b8a83cd6593ea11661ff63dfcfe51009d1359810
rahul <iamrahulparekh@gmail.com>
2021-04-13 19:15:01 -0600

    Initial Commit

---
+++ RecipeApp/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  RecipeApp
+//
+//  Created by Rahul Parekh on 2021-04-13.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RecipeListView()
+        }
+    }
+}
---
+++ RecipeApp/Views/RecipeListView.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeListView.swift
+//  RecipeApp
+//
+//  Created by Rahul Parekh on 2021-04-13.
+//
+
+import SwiftUI
+
+struct RecipeListView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct RecipeListView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeListView()
+    }
+}
